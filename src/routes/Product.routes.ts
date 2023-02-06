7bb16dc02eccbcc92f9060983527a42839ac32b6
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-02-06 15:30:54 -0300

    feat: endpoint para o cadastro de produtos

---
+++ src/routes/Product.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+import ProductController from '../controllers/ProductController';
+
+const router = Router();
+
+const controller = new ProductController();
+
+router.post('/', controller.create);
+
+export default router;
